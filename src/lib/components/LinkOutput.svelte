037ddb9c9ac4fbdc53f91b018ccbcabf88cb8f3a
mjlinane <19750620+mj-linane@users.noreply.github.com>
2023-03-24 14:14:09 -0400

    initial

---
+++ src/lib/components/LinkOutput.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	let downloadLink: string;
+</script>
+
+<div class="mt-4 bg-white p-10 text-center rounded-md">
+	<h2 class="font-medium text-xl mb-4 text-primary">Your Link</h2>
+	<p class="text-accent">{downloadLink}</p>
+</div>

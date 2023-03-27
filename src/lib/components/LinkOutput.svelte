43b58e2aad4e9337c1a71b6a8fca51a4b994061c
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-03-27 09:54:00 -0400

    fix bug with passing props

--- src/lib/components/LinkOutput.svelte
+++ src/lib/components/LinkOutput.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	let downloadLink: string;
+	export let link: string;
 </script>
 
 <div class="mt-4 bg-white p-10 text-center rounded-md">
 	<h2 class="font-medium text-xl mb-4 text-primary">Your Link</h2>
-	<p class="text-accent">{downloadLink}</p>
+	<p class="text-accent">{link}</p>
 </div>
